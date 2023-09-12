a20d30daf76a02293162a2cb13571960a4c43ad1
Antinii <klein.antony@hotmail.fr>
2023-09-12 14:06:07 +0200

    Fin phase 1, scraping d'un seul livre

--- Klein_Antony_1_Projet2_092023.py
+++ Klein_Antony_1_Projet2_092023.py
@@ -3,34 +3,58 @@ import csv
 import requests
 from bs4 import BeautifulSoup
 
-url = 'https://books.toscrape.com/catalogue/dune-dune-1_151/'
-response = requests.get(url)
+# PHASE 1
+url_dune = 'https://books.toscrape.com/catalogue/dune-dune-1_151/'
+response = requests.get(url_dune)
 
+# Scraping des données demandées pour l'ouvrage selectionné
+headers = ['url page', 'upc', 'titre', 'prix avec taxes', 'prix sans taxes', 'quantité en stock', 'description',
+           'catégorie', 'note avis', 'url image']
 dune_book = []
 if response.ok:
     soup = BeautifulSoup(response.content, 'html.parser')
     title = soup.find('div', {'class': 'col-sm-6 product_main'}).find('h1')
-    p = soup.find_all('p')
     td = soup.find_all('td')
     upc = td[0].get_text()
     price_excluding_taxes = td[2].get_text()
     price_including_taxes = td[3].get_text()
     stock = td[5].get_text()
-    description = p[3].get_text()
-    reviews = td[6].get_text()
+    description = soup.find('article', {'class': 'product_page'}).find_all('p')
+    category = soup.find('li', {'class': 'active'}).find_previous('li')
+    rating = soup.find('i', {'class': 'icon-star'}).find_parent('p').get('class')
     find_img_url = soup.find('div', {'class': 'item active'}).find('img').attrs['src']
     img_url = 'https://books.toscrape.com/'+find_img_url.replace("../../", "")
     data = {
-        'url': url,
+        'url page': url_dune,
         'upc': upc,
         'titre': title.text,
-        'prix sans taxes': price_excluding_taxes,
         'prix avec taxes': price_including_taxes,
-        'stock': stock,
-        'description': description,
-        'nombre avis': reviews,
-        'lien de l image': img_url
+        'prix sans taxes': price_excluding_taxes,
+        'quantité en stock': stock,
+        'description': description[3].text,
+        'catégorie': category.text.strip(),
+        'note avis': (rating[1].lower() + ' out of five'),
+        'url image': img_url
     }
     dune_book.append(data)
 
-print(dune_book)
+# Extraction des données vers fichier CSV
+with open('dune_book.csv', 'w', newline='') as file:
+    writer = csv.DictWriter(file, fieldnames=headers)
+    writer.writeheader()
+    writer.writerows(dune_book)
+
+
+# PHASE 2
+# Extraction des url de la catégorie science fiction
+url_category = 'https://books.toscrape.com/catalogue/category/books/science-fiction_16/index.html'
+response = requests.get(url_category)
+
+if response.ok:
+    cat_links = []
+    soup = BeautifulSoup(response.text, 'html.parser')
+    all_links = soup.find_all('h3')
+    for h3 in all_links:
+        a = h3.find('a')
+        link = 'https://books.toscrape.com/catalogue/' + a['href'].replace('../../../', '')
+        cat_links.append(link)
